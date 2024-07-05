0607b1b5af0b91f05f8ebedcbf00c4566eb5f9f6
Piotr Jakubiec <pjakubiec0@gmail.com>
2024-07-05 16:43:44 +0200

    Added isbn not required

--- Models/Book.cs
+++ Models/Book.cs
@@ -6,6 +6,6 @@
         public string Title { get; set; }
         public string Author { get; set; }
         public int Year { get; set; }
-        public string ISBN { get; set; }
+        public string? ISBN { get; set; }
     }
 }
